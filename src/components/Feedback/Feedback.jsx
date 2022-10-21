80e3a062be531451281bc77cdaa28184146e49cb
ancorframe <yura.pundiak@gmail.com>
2022-10-21 15:42:26 +0300

    +

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,68 +1,53 @@
-import React from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions';
 import { FeedbackStatistics } from './FeedbackStatistics';
 import { Title } from './Feedback.styled';
 
-export class Feedback extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  const state = { good, bad, neutral };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
-
-  countPositiveFeedbackPercentage() {
+  const countPositiveFeedbackPercentage = () => {
     return Math.round(
-      this.countTotalFeedback() &&
-        (this.state.good / this.countTotalFeedback()) * 100
+      countTotalFeedback() && (good / countTotalFeedback()) * 100
     );
-  }
-
-  onHandleGood = () => {
-    this.setState(prevState => {
-      return {
-        good: prevState.good + 1,
-      };
-    });
-  };
-  onHandleNeutral = () => {
-    this.setState(prevState => {
-      return {
-        neutral: prevState.neutral + 1,
-      };
-    });
-  };
-  onHandleBad = () => {
-    this.setState(prevState => {
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
   };
 
-  render() {
-    return (
-      <div>
-        <Title>Please leave feedback</Title>
-        <FeedbackOptions
-          onHandleGood={this.onHandleGood}
-          onHandleNeutral={this.onHandleNeutral}
-          onHandleBad={this.onHandleBad}
+  const onHandleGood = () => {
+    setGood(prev => prev + 1);
+  };
+  const onHandleNeutral = () => {
+    setNeutral(prev => prev + 1);
+  };
+  const onHandleBad = () => {
+    setBad(prev => prev + 1);
+  };
+
+  return (
+    <div>
+      <Title>Please leave feedback</Title>
+      <FeedbackOptions
+        onHandleGood={onHandleGood}
+        onHandleNeutral={onHandleNeutral}
+        onHandleBad={onHandleBad}
+      />
+      <Title>Statistics</Title>
+      {countTotalFeedback() ? (
+        <FeedbackStatistics
+          state={state}
+          TotalFeedback={countTotalFeedback()}
+          PositiveFeedbackPercentage={countPositiveFeedbackPercentage()}
         />
-        <Title>Statistics</Title>
-        {this.countTotalFeedback() ? (
-          <FeedbackStatistics
-            state={this.state}
-            TotalFeedback={this.countTotalFeedback()}
-            PositiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <p>There is no feedback</p>
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <p>There is no feedback</p>
+      )}
+    </div>
+  );
+};
